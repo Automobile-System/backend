de9cbc2f516f33db24e78f51fed54dd38182a698
Thanuka Perera <155462762+ThanukaPerera@users.noreply.github.com>
2025-09-28 07:39:48 +0530

    Database connection is okay

--- src/main/java/com/TenX/Automobile/AutomobileApplication.java
+++ src/main/java/com/TenX/Automobile/AutomobileApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AutomobileApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AutomobileApplication.class, args);
+
+        SpringApplication.run(AutomobileApplication.class, args);
 	}
 
 }
