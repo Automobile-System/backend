069f785aa6c1b8c349f46bb81daf4d4812639163
Adithya Hewage <adithyahewage69@gmail.com>
2025-11-04 18:16:56 +0530

    commited

--- src/main/java/com/TenX/Automobile/config/SecurityConfig.java
+++ src/main/java/com/TenX/Automobile/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.TenX.Automobile.config;
 
-import com.TenX.Automobile.security.JwtAuthenticationFilter;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,8 +20,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.List;
+import com.TenX.Automobile.security.JwtAuthenticationFilter;
 
 /**
  * Enterprise-level Security Configuration with role-based access control
